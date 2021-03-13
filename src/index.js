05ff176cd6c1bf16bf18a1c465f4cccc6891d082
vladimirkononovich-main <vova.kononovich.main@gmail.com>
2021-03-13 21:58:17 +0300

    feat: change

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-    return Math.min(...array);  
+    if(array == undefined || array.length == 0) {
+      return 0;
+    } else {
+      return Math.min(...array);
+    } 
 }
 
 exports.max = function max (array) {
-  return Math.max(...array);
+  
+  if(array == undefined || array.length == 0) {
+    return 0;
+  } else {
+    return Math.max(...array);
+  }
 }
 
 exports.avg = function avg (array) {
-    return array.reduce((a,b) => a + b, 0) / array.length;  
+     
+    if(array == undefined || array.length == 0) {
+      return 0;
+    } else {
+      return array.reduce((a,b) => a + b, 0) / array.length; 
+    }
 }
