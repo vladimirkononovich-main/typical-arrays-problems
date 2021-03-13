613d6b6ecfc2ed5e3a213c4ca422d829a92a7cd6
vladimirkononovich-main <vova.kononovich.main@gmail.com>
2021-03-13 21:27:15 +0300

    change

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+    return Math.min(...array);  
 }
 
 exports.max = function max (array) {
-  return 0;
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-    return array.reduce((a,b) => a + b, 0) / arr.length
-    
+    return array.reduce((a,b) => a + b, 0) / array.length;  
 }
