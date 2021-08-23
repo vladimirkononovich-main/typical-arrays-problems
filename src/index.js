b90af668c31c6fa4ad524874f68707e1d58b0633
vladimirkononovich-main <vova.kononovich.main@gmail.com>
2021-08-23 04:35:50 +0300

    feat: add min max avg func

--- src/index.js
+++ src/index.js
@@ -1,26 +1,40 @@
+// exports.min = function min (array) {
+//     if(array == undefined || array.length == 0) {
+//       return 0;
+//     } else {
+//       return Math.min(...array);
+//     }
+// }
 
-exports.min = function min (array) {
-    if(array == undefined || array.length == 0) {
-      return 0;
-    } else {
-      return Math.min(...array);
-    } 
-}
+// exports.max = function max (array) {
 
-exports.max = function max (array) {
-  
-  if(array == undefined || array.length == 0) {
-    return 0;
-  } else {
-    return Math.max(...array);
-  }
-}
+//   if(array == undefined || array.length == 0) {
+//     return 0;
+//   } else {
+//     return Math.max(...array);
+//   }
+// }
 
-exports.avg = function avg (array) {
-     
-    if(array == undefined || array.length == 0) {
-      return 0;
-    } else {
-      return array.reduce((a,b) => a + b, 0) / array.length; 
-    }
-}
+// exports.avg = function avg (array) {
+
+//     if(array == undefined || array.length == 0) {
+//       return 0;
+//     } else {
+//       return array.reduce((a,b) => a + b, 0) / array.length;
+//     }
+// }
+
+exports.min = function min(array) {
+    if (array == undefined || array.length == 0) return 0;
+    else return Math.min(...array);
+};
+
+exports.max = function max(array) {
+  if (array == undefined || array.length == 0) return 0;
+  else return Math.max(...array);
+};
+
+exports.avg = function avg(array) {
+  if (array == undefined || array.length == 0) return 0;
+  else return array.reduce((a,b) => a + b, 0) / array.length;
+};
